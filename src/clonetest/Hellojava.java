f860c97e2d3612e19c92fb61d56c4f7fd0733c97
prakesh2539@gmail.com <prakesh2539@gmail.com>
2025-03-06 22:51:41 +0530

    Changes Checking

--- src/clonetest/Hellojava.java
+++ src/clonetest/Hellojava.java
@@ -5,7 +5,10 @@ public class Hellojava {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hello java");
+	//	System.out.println("Hello java");
+		
+		
+		System.out.println("Hello Rakesh");
 	}
 
 }
